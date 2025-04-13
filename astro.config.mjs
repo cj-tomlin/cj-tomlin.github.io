cdc25845376c0658d2668ff4c2a332707434634c
Chris Tomlin <tomlinc@proton.me>
2025-04-13 22:40:37 +0100

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import staticAdapter from '@astrojs/static';
 
 export default defineConfig({
 	site: 'https://cj-tomlin.github.io',
 	base: '/portfolio/',
-	adapter: staticAdapter(),
 	experimental: {
 		svg: true,
 	},
